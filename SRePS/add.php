ddc24de642c04cbb8fcad912bad9785af97fb51f
ZackChung <zackchung98@gmail.com>
2020-09-30 14:26:48 +1000

    add a record function

---
+++ SRePS/add.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description", content="PHP SRePS" />
+	<title>SRePS</title>
+</head>
+<body>
+	<h1>Add a Sales Record</h1>
+	<form method="post" action="action.php">
+		<label for="scode">Sales Code:</label><br/>
+		<input type="text" name="scode" pattern="[0-9]+" required="required" /><br/>
+		<label for="pcode">Product Code:</label><br/>
+		<input type="text" name="pcode" required="required" /><br/>
+		<label for="quantity">Quantity:</label><br/>
+		<input type="text" name="quantity" pattern="[0-9]+" required="required" /><br/>
+		<label for="date">Date of Sold:</label><br/>
+		<input type="date" name="date" required="required" /><br/>
+		<input type="submit" value="Add" />
+	</form>
+</body>
+</html>
---
+++ SRePS/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description", content="PHP SRePS" />
+	<title>SRePS</title>
+</head>
+<body>
+	<h1>Sales Report and Predict System</h1>
+	<button><a href="add.php" title="add record">Add a Sales Record</a></button>
+	<button><a href="display.php" title="display record">Display a Sales Record</a></button>
+</body>
+</html>
