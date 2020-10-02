a9ae0493b3661f8c2e15a6f1b07e8d9289f355d6
benmcconnonswin <102119982@student.swin.edu.au>
2020-10-02 12:38:18 +1000

    Added report display page

---
+++ SRePS/report.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description", content="PHP SRePS" />
+	<title>SRePS - Generate Report</title>
+</head>
+
+<body>
+	<h1>Generate a Sales Report</h1>
+	<form method="post" action="report_page.php">
+        <label for="databegin">Date Range Begin:</label><br/>
+        <input type="date" name="datebegin" required="required" /><br/>
+        <label for="dataend">Date Range End:</label><br/>
+        <input type="date" name="dateend" required="required" /><br/>
+        <input type="submit" value="Generate" />
+    </form>
+    <button><a href ="index.php">Go Back</a></button>
+</body>
