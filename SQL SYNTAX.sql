11d3515b59ba51ee4721b476c455662605d77e9a
gav427 <102111726@student.swin.edu.au>
2020-10-09 16:26:31 +1100

    adding sql file

---
+++ SQL SYNTAX.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE	Pharmacy;
+
+USE Pharmacy;
+
+CREATE TABLE Product(
+  Product_Code INT(20) NOT NULL AUTO_INCREMENT,
+  Product_Name VARCHAR(100) NOT NULL,
+  Stock INT(255) NOT NULL,
+  Sales_Price FLOAT(2) NOT NULL,
+  Bought_Price FLOAT(2) NOT NULL,
+  PRIMARY KEY (Product_Code)
+);
+
+CREATE TABLE Sales(
+  Sales_Code INT(20) NOT NULL AUTO_INCREMENT,
+  Product_Code INT(20) NOT NULL,
+  Sales_Quantity INT(100) NOT NULL,
+  Date_Of_Purchase DATE NOT NULL,
+  PRIMARY KEY (Sales_Code),
+  FOREIGN KEY (Product_Code) REFERENCES Product(Product_Code)
+);
+
+
