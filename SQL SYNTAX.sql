c4ee6188bb68c51ec26255637641d3e5f9ed1176
gav427 <102111726@student.swin.edu.au>
2020-10-10 18:17:30 +1100

    Adding dummy data for database

--- SQL SYNTAX.sql
+++ SQL SYNTAX.sql
@@ -20,4 +20,18 @@ CREATE TABLE Sales(
   FOREIGN KEY (Product_Code) REFERENCES Product(Product_Code)
 );
 
+INSERT INTO Product (Product_Name,Stock,Sales_Price,Bought_Price)
+VALUES 				("Pain Relief 20 Capsules",50,6.09,3.00),
+					("Cough & Cold Relief 200ml",50,12.75,4.50),
+					("Fat Burner 10 tablets",20,79.99,20.00),
+					("General Vitamins 150 tablets",45,24.99,5.60);
+INSERT INTO Sales (Product_Code,Sales_Quantity,Date_Of_Purchase)
+VALUES				(4,1,"2020-10-10"),
+					(4,2,"2020-10-10"),
+					(1,1,"2020-10-10"),
+					(2,1,"2020-10-10"),
+					(2,1,"2020-10-10"),
+					(3,3,"2020-10-10"),
+					(4,1,"2020-10-10"),
+					(1,1,"2020-10-10");
 
